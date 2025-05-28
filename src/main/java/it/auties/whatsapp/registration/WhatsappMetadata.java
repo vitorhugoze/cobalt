c3ba45d5fc03c7cd0192ae171069a1cf4708329f
vitor <vitorhugozeferino@hotmail.com>
2025-05-28 16:00:05 -0300

    Ajuste pegar versão

--- src/main/java/it/auties/whatsapp/registration/WhatsappMetadata.java
+++ src/main/java/it/auties/whatsapp/registration/WhatsappMetadata.java
@@ -16,82 +16,89 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.security.SecureRandom;
 import java.util.Base64;
 import java.util.HexFormat;
 import java.util.concurrent.CompletableFuture;
+import java.util.regex.Pattern;
 
 public final class WhatsappMetadata {
-    private static final Version MOBILE_BUSINESS_IOS_VERSION = Version.of("2.24.16.78");
-    private static final Version MOBILE_PERSONAL_IOS_VERSION = Version.of("2.24.17.71");
-    private static final URI WEB_UPDATE_URL = URI.create("https://web.whatsapp.com/check-update?version=2.2245.9&platform=web");
-    private static final String MOBILE_IOS_STATIC = "0a1mLfGUIBVrMKF1RdvLI5lkRBvof6vn0fD2QRSM";
-    private static final String MOBILE_BUSINESS_IOS_STATIC = "USUDuDYDeQhY4RF2fCSp5m3F6kJ1M2J8wS7bbNA2";
+	private static final Version MOBILE_BUSINESS_IOS_VERSION = Version.of("2.24.16.78");
+	private static final Version MOBILE_PERSONAL_IOS_VERSION = Version.of("2.24.17.71");
+	private static final URI WEB_UPDATE_URL = URI.create("https://web.whatsapp.com");
+	private static final String MOBILE_IOS_STATIC = "0a1mLfGUIBVrMKF1RdvLI5lkRBvof6vn0fD2QRSM";
+	private static final String MOBILE_BUSINESS_IOS_STATIC = "USUDuDYDeQhY4RF2fCSp5m3F6kJ1M2J8wS7bbNA2";
+	private static final String MOBILE_WEB_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36";
+	private static final Pattern WEB_UPDATE_PATTERN = Pattern.compile("\"client_revision\":(\\w+)", Pattern.MULTILINE);
+	private static final Version WEB_VERSION = Version.of("2.3000.1022032575");
 
-    private static volatile Version webVersion;
+	private static volatile Version webVersion;
 
-    public static CompletableFuture<Version> getVersion(PlatformType platform) {
-        return switch (platform) {
-            case WEB, WINDOWS, MACOS ->
-                    getWebVersion();
-            case IOS ->
-                    CompletableFuture.completedFuture(MOBILE_PERSONAL_IOS_VERSION);
-            case IOS_BUSINESS ->
-                    CompletableFuture.completedFuture(MOBILE_BUSINESS_IOS_VERSION);
-            default -> throw new IllegalStateException("Unsupported mobile os: " + platform);
-        };
-    }
+	public static CompletableFuture<Version> getVersion(PlatformType platform) {
+		return switch (platform) {
+		case WEB, WINDOWS, MACOS -> getWebVersion();
+		case IOS -> CompletableFuture.completedFuture(MOBILE_PERSONAL_IOS_VERSION);
+		case IOS_BUSINESS -> CompletableFuture.completedFuture(MOBILE_BUSINESS_IOS_VERSION);
+		default -> throw new IllegalStateException("Unsupported mobile os: " + platform);
+		};
+	}
 
-    private static CompletableFuture<Version> getWebVersion() {
-        if (webVersion != null) {
-            return CompletableFuture.completedFuture(webVersion);
-        }
+	private static CompletableFuture<Version> getWebVersion() {
+		if (webVersion != null) {
+			return CompletableFuture.completedFuture(webVersion);
+		}
 
-        try(var client = HttpClient.newHttpClient()) {
-            var request = HttpRequest.newBuilder()
-                    .uri(WEB_UPDATE_URL)
-                    .build();
-            return client.sendAsync(request, HttpResponse.BodyHandlers.ofByteArray()).thenApplyAsync(response -> {
-                var webVersionResponse = Json.readValue(response.body(), WebVersionResponse.class);
-                return webVersion = Version.of(webVersionResponse.currentVersion());
-            });
-        }
-    }
+		try (var client = HttpClient.newHttpClient()) {
+			var request = HttpRequest.newBuilder().uri(WEB_UPDATE_URL).header("User-Agent", MOBILE_WEB_USER_AGENT)
+					.header("Accept",
+							"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+					.header("Accept-Language", "en-US,en;q=0.9").header("Sec-Fetch-Dest", "document")
+					.header("Sec-Fetch-Mode", "navigate").header("Sec-Fetch-Site", "none")
+					.header("Sec-Fetch-User", "?1").build();
+			return client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenApplyAsync(response -> {
+				if (response.statusCode() != 200) {
+					return WEB_VERSION;
+				}
 
-    public static CompletableFuture<String> getToken(long phoneNumber, PlatformType platform, Version appVersion) {
-        return switch (platform) {
-            case IOS, IOS_BUSINESS -> getIosToken(phoneNumber, appVersion, platform.isBusiness());
-            default -> throw new IllegalStateException("Unsupported mobile os: " + platform);
-        };
-    }
+				return WEB_UPDATE_PATTERN.matcher(response.body()).results().findFirst().map(entry -> {
+					try {
+						var clientVersion = Integer.parseUnsignedInt(entry.group(1));
+						return webVersion = new Version(2, 3000, clientVersion);
+					} catch (Throwable throwable) {
+						return WEB_VERSION;
+					}
+				}).orElse(WEB_VERSION);
+			});
+		}
+	}
 
-    private static CompletableFuture<String> getIosToken(long phoneNumber, Version version, boolean business) {
-        var staticToken = business ? MOBILE_BUSINESS_IOS_STATIC : MOBILE_IOS_STATIC;
-        var token = staticToken + HexFormat.of().formatHex(version.toHash()) + phoneNumber;
-        return CompletableFuture.completedFuture(HexFormat.of().formatHex(MD5.calculate(token)));
-    }
+	public static CompletableFuture<String> getToken(long phoneNumber, PlatformType platform, Version appVersion) {
+		return switch (platform) {
+		case IOS, IOS_BUSINESS -> getIosToken(phoneNumber, appVersion, platform.isBusiness());
+		default -> throw new IllegalStateException("Unsupported mobile os: " + platform);
+		};
+	}
 
-    public static String generateBusinessCertificate(Keys keys) {
-        var details = new BusinessVerifiedNameDetailsBuilder()
-                .name("")
-                .issuer("smb:wa")
-                .serial(Math.abs(new SecureRandom().nextLong()))
-                .build();
-        var encodedDetails = BusinessVerifiedNameDetailsSpec.encode(details);
-        var certificate = new BusinessVerifiedNameCertificateBuilder()
-                .encodedDetails(encodedDetails)
-                .signature(Curve25519.sign(keys.identityKeyPair().privateKey(), encodedDetails, true))
-                .build();
-        return Base64.getUrlEncoder().encodeToString(BusinessVerifiedNameCertificateSpec.encode(certificate));
-    }
+	private static CompletableFuture<String> getIosToken(long phoneNumber, Version version, boolean business) {
+		var staticToken = business ? MOBILE_BUSINESS_IOS_STATIC : MOBILE_IOS_STATIC;
+		var token = staticToken + HexFormat.of().formatHex(version.toHash()) + phoneNumber;
+		return CompletableFuture.completedFuture(HexFormat.of().formatHex(MD5.calculate(token)));
+	}
 
+	public static String generateBusinessCertificate(Keys keys) {
+		var details = new BusinessVerifiedNameDetailsBuilder().name("").issuer("smb:wa")
+				.serial(Math.abs(new SecureRandom().nextLong())).build();
+		var encodedDetails = BusinessVerifiedNameDetailsSpec.encode(details);
+		var certificate = new BusinessVerifiedNameCertificateBuilder().encodedDetails(encodedDetails)
+				.signature(Curve25519.sign(keys.identityKeyPair().privateKey(), encodedDetails, true)).build();
+		return Base64.getUrlEncoder().encodeToString(BusinessVerifiedNameCertificateSpec.encode(certificate));
+	}
 
-    private record WebVersionResponse(@JsonProperty("isBroken") boolean broken,
-                                      @JsonProperty("isBelowSoft") boolean outdatedSoft,
-                                      @JsonProperty("isBelowHard") boolean outdatedHard,
-                                      @JsonProperty("hardUpdateTime") long outdatedUpdateTime,
-                                      @JsonProperty("beta") String beta,
-                                      @JsonProperty("currentVersion") String currentVersion) {
+	private record WebVersionResponse(@JsonProperty("isBroken") boolean broken,
+			@JsonProperty("isBelowSoft") boolean outdatedSoft, @JsonProperty("isBelowHard") boolean outdatedHard,
+			@JsonProperty("hardUpdateTime") long outdatedUpdateTime, @JsonProperty("beta") String beta,
+			@JsonProperty("currentVersion") String currentVersion) {
 
-    }
+	}
 }
